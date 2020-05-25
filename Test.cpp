8842b05deafd63cfa42c9bca7524e2f27652dbb5
Dean <carmidean@gmail.com>
2020-05-25 20:30:15 +0300

    test

---
+++ Test.cpp
@@ -0,0 +1,15 @@
+#include "doctest.h"
+
+#include "FootSoldier.hpp"
+#include "FootCommander.hpp"
+#include "Sniper.hpp"
+#include "Paramedic.hpp"
+#include "SniperCommander.hpp"
+#include "ParamedicCommander.hpp"
+
+#include "DemoGame.hpp"
+
+namespace WarGame{
+
+    
+}

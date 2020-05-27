7241c5f56f9b403ec0814728edbaa90218155a67
Dean <carmideanGmail.com>
2020-05-27 18:01:11 +0300

    dd

--- Soldier.hpp
+++ Soldier.hpp
@@ -25,26 +25,6 @@ public:
     virtual void hit(std::vector<std::vector<Soldier *>> &board, std::pair<int, int> source) = 0;
 
     virtual void heal() = 0;
-    //     void print_board(std::vector<std::vector<Soldier*>> &board,std::pair<int,int> source){
-    //     std::cout << std::fixed << std::setprecision(2) << std::setfill('0');
-    //     for(int i=0; i<board.size();i++){
-    //         for(int j=0; j< board.size();j++){
-    //             if( i == source.first && j == source.second){
-    //                     std::cout << "||" << std::setw(9) << "!!!!!!!!!";
-    //             }
-    //             else if (board[i][j]==0)
-    //             {
-    //                 std::cout << "||" << std::setw(9)<< "000000000";
-    //             }
-    //             else
-    //             {
-    //                 std::cout << "||" << std::setw(9)<<  board[i][j];
-    //             }
-
-    //         }
-    //         std::cout << "||" << std::endl;
-    //     }
-    //     std::cout << "" << std::endl;
-    // }
+    
     virtual ~Soldier() {}
 };
